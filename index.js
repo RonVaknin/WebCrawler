c0ec4cda64359d62c11bdd10090bb59953fc87b7
Ron Vaknin <57563969+RonVaknin@users.noreply.github.com>
2022-03-29 21:09:10 +0300

    get image urls from specified site

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const myArgs = process.argv.slice(2);
 const main_url = myArgs[0];
 const depth = myArgs[1] ?? 3;
-const image_reg = /<img ([\w\W]+?)\/>/g;
+const image_reg = new RegExp(/\<img.+src\=(?:\"|\')(.+?)(?:\"|\')(?:.+?)\>/, 'g');
 
 let current_depth = 0;
 //TODO:: validate url?
@@ -15,10 +15,18 @@ if(isNaN(depth)) {
 axios.get(myArgs[0])
     .then(function (response) {
         console.log("proccessing");
-        let body = response.data.split(image_reg);
-        console.log(body[1]);
+        var m,
+        urls = [], 
+        str = response.data,
+        rex = /<img[^>]+src="([^">]+)/g;
+    
+        while ( m = rex.exec( str ) ) {
+            urls.push( m[1] );
+        }
+    
+        console.log( urls ); 
+    })
+    .catch(function(error){
+        console.error("Failed to get " + main_url);
     });
-    // .catch(function(error){
-    //     console.error("Failed to get " + main_url);
-    // });
 
