0253df74f5e7cd0ed19f08e5f8afcb7c7eca1d38
Ron Vaknin <57563969+RonVaknin@users.noreply.github.com>
2022-03-29 21:43:19 +0300

    write to file

--- index.js
+++ index.js
@@ -1,32 +1,60 @@
 const axios = require('axios');
+const fs = require('fs');
 
-const myArgs = process.argv.slice(2);
-const main_url = myArgs[0];
-const depth = myArgs[1] ?? 3;
+const argv = process.argv.slice(2);
+const main_url = argv[0];
+const depth = argv[1] ?? 3;
 const image_reg = new RegExp(/\<img.+src\=(?:\"|\')(.+?)(?:\"|\')(?:.+?)\>/, 'g');
 
-let current_depth = 0;
+let results = [];
+
 //TODO:: validate url?
 
 if(isNaN(depth)) {
     console.log("Second input must be an integer");
 }
 
-axios.get(myArgs[0])
+
+scan(argv[0], 0);
+
+async function start(){
+    await scan(argv[0], 0);
+    console.log(results);
+}
+
+async function scan(url, current_depth) {
+    if(current_depth >= depth){
+        return;
+    }
+
+    await axios.get(url)
     .then(function (response) {
         console.log("proccessing");
         var m,
         urls = [], 
         str = response.data,
         rex = /<img[^>]+src="([^">]+)/g;
-    
-        while ( m = rex.exec( str ) ) {
-            urls.push( m[1] );
-        }
-    
-        console.log( urls ); 
-    })
-    .catch(function(error){
-        console.error("Failed to get " + main_url);
-    });
 
+        while ( m = rex.exec( str ) ) {
+            result = {
+                imageUrl: m[1],
+                sourceUrl: url,
+                depth: current_depth
+            }
+            urls.push(result);
+        }
+        return JSON.stringify(urls);
+    })
+    .then(function (urls) {
+        //TODO: generate hashfile or date filename for every result
+        fs.writeFile(__dirname + "/urls.txt", urls, err => {
+            if (err) {
+            console.error(err)
+            return
+            }
+        }
+    )});
+    // .catch(function(error){
+    //     console.error("Failed to get " + main_url);
+    // });
+}
