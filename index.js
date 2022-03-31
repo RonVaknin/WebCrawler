113d91d5fdd2bd558fe1b8cacfbfdc2d9f96e3fd
Ron Vaknin <57563969+RonVaknin@users.noreply.github.com>
2022-03-31 23:39:09 +0300

    nothing important

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const fs = require('fs');
 const argv = process.argv.slice(2);
 const main_url = argv[0];
 const depth = argv[1] ?? 3;
-const image_reg = new RegExp(/\<img.+src\=(?:\"|\')(.+?)(?:\"|\')(?:.+?)\>/, 'g');
+const image_regex = /<img[^>]+src="([^">]+)/g;
+const url_regex = /<a[^>]+href="http([^">]+)/g;
 
 let results = [];
 
@@ -13,7 +14,6 @@ let results = [];
 if(isNaN(depth)) {
     console.log("Second input must be an integer");
 }
-
 
 start();
 
@@ -42,10 +42,9 @@ async function scan(url, current_depth) {
     await axios.get(url)
     .then(function (response) {
         let image_url,
-        str = response.data,
-        rex = /<img[^>]+src="([^">]+)/g;
+        str = response.data;
 
-        while ( image_url = rex.exec( str ) ) {
+        while ( image_url = image_regex.exec( str ) ) {
             result = {
                 imageUrl: image_url[1],
                 sourceUrl: url,
@@ -63,14 +62,13 @@ async function scan(url, current_depth) {
         
         str = response.data;
         //TODO: current regex only get http links, might want to get local path too and append current url.
-        rex = /<a[^>]+href="http([^">]+)/g;
 
-        while ( href_link = rex.exec( str ) ) {
-            let link = "http" + href_link[1]; 
-            scan(link, ++current_depth);
+        while ( href_link = url_regex.exec( str ) ) {
+            scan("http" + href_link[1], ++current_depth);
         }
     })
     .catch(function(error){
         console.error("Failed to get " + main_url);
     });
 }
+
